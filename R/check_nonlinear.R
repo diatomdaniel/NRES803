20ebcb5ed2474c35bc079ef85b26abc76c128f6c
diatomdaniel <daniel.gschwentner@gmail.com>
2020-11-04 17:09:27 +0100

    Fixed check_linear

--- R/check_nonlinear.R
+++ R/check_nonlinear.R
@@ -1,5 +1,3 @@
-if(getRversion() >= "2.15.1")  
-  
 #' Plot model parameters against residuals of a fitted model
 #'
 #'
@@ -22,20 +20,63 @@ check_nonlinear <- function(x,...){
   # Get model data. 
   x_dat <- model.frame(x)
   
+  # Find the response variable. 
+  response <- attr(terms(x), "response")
+  
+  # Drop it from data frame. 
+  x_dat <- x_dat[,-response, drop = FALSE]
+  
+  # Get class information. 
+  x_dat_class <- sapply(x_dat, class)
+  
   # Get model residuals. 
   x_dat$residuals <- residuals(x, type = "pearson")
   
-  # Pivot longer using tidyverse. 
-  x_dat_gather <- tidyr::gather(x_dat, "Variables", "values", -residuals)
+  # Get variables names. 
+  x_dat_names <- names(x_dat)
   
-  # Plot data. 
-  ggplot2::ggplot(x_dat_gather, ggplot2::aes(values, residuals)) + 
-    ggplot2::geom_hline(yintercept = 0, col = "red") +
-    ggplot2::geom_point() + 
-    ggplot2::geom_smooth(method = "loess", formula = y~x, se = FALSE) + 
-    ggplot2::facet_wrap(Variables~., scales = "free") + 
-    ggplot2::labs(x = "Variables", y = "Pearson residuals") + 
-    my_theme
+  # For loop for plots.
+  # Storage list. 
+  plotlist <- list()
+  # For loop for columns in df. 
+  for(i in seq_along(x_dat_class)){
+    # Treat numeric and factor seperately. 
+    if(x_dat_class[i] == "numeric"){
+      
+      # Continuous variable. 
+      plot_ob <- ggplot2::ggplot(x_dat, ggplot2::aes_string(x_dat_names[i], "residuals")) + 
+        ggplot2::geom_hline(yintercept = 0, col = "red") +
+        ggplot2::geom_point() + 
+        ggplot2::geom_smooth(method = "loess", formula = y~x, se = FALSE) + 
+        ggplot2::labs(x = colnames(x_dat)[i], y = "Pearson Residuals") + 
+        my_theme
+      
+      # Append plot. 
+      plotlist[[i]] <- plot_ob
+      
+    } else{ # Factor. 
+      plot_ob <- ggplot2::ggplot(x_dat, ggplot2::aes_string(x_dat_names[i], "residuals")) + 
+        ggplot2::geom_hline(yintercept = 0, col = "red") +
+        ggplot2::geom_boxplot() + 
+        ggplot2::labs(x = colnames(x_dat)[i], y = "Pearson Residuals") + 
+        my_theme
+      
+      # Append plot. 
+      plotlist[[i]] <- plot_ob
+      
+      
+      
+    }
+    
+    
+  }
+  
+  ggpubr::ggarrange(plotlist =plotlist)
+  
 
 }
 
+# Reproducible example. 
+#data("iris")
+#x <- lm(Sepal.Length ~ ., data = iris)
+#check_nonlinear(x)
